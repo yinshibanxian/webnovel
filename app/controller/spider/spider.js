c59b3b277c3ef8ca636a995351e3fd9810600806
steventu <steventu@wezhuiyi.com>
2019-11-17 20:04:56 +0800

    refactor: 重构代码，将爬取小说列表程序放在service层

--- app/controller/spider/spider.js
+++ app/controller/spider/spider.js
@@ -1,31 +1,8 @@
 const Controller = require('egg').Controller;
-const cheerio = require('cheerio');
 
 class SpiderController extends Controller {
     async index() {
-        const {ctx} = this;
-        // 获取全部小说页面结构
-        const res = await ctx.service.spider.bookList.getBookList();
-        const html = res.data;
-        const $ = cheerio.load(html);
-        const novelList = $('div#main div.novellist ul>li');
-        const novelListArray = Array.from(novelList);
-        const novelListToSave = [];
-        const novelListModel = this.ctx.model.Novellist;
-        novelListArray.forEach(async (novel) => {
-            // 书籍链接
-            const link = novel.children[0].attribs.href;
-            // 书籍名称
-            const novelName = novel.children[0].children[0].data;
-            const novelItem = new novelListModel({link,novelName});
-            novelListToSave.push(novelItem);
-        })
-        const result = await novelListModel.insertMany(novelListToSave);
-        console.log(result);
-        
-
-        
-        
+        await this.ctx.service.spider.bookList.getBookList();
     }
 }
 
--- app/service/spider/bookList.js
+++ app/service/spider/bookList.js
@@ -1,9 +1,27 @@
 const Service = require('egg').Service;
 const axios = require('axios');
-
+const cheerio = require('cheerio');
 class BookListService extends Service {
     async getBookList() {
-        return await axios.get('http://www.xbiquge.la/xiaoshuodaquan/');
+        // 获取全部小说页面结构
+        const res = await axios.get('http://www.xbiquge.la/xiaoshuodaquan/');
+        const html = res.data;
+        const $ = cheerio.load(html);
+        const novelList = $('div#main div.novellist ul>li');
+        const novelListArray = Array.from(novelList);
+        const novelListModel = this.ctx.model.Novellist;
+        novelListArray.forEach(async (novel) => {
+            // 书籍链接
+            const link = novel.children[0].attribs.href;
+            // 书籍名称
+            const novelName = novel.children[0].children[0].data;
+            const foundResult = await novelListModel.find({novelName});
+            //如果小说列表中没有对应的小说，再增加
+            if(foundResult.length === 0) {
+                const novelItem = new novelListModel({link,novelName}); 
+                await novelItem.save();
+            }
+        })
     }
 }
 
